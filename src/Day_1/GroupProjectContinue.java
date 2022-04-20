4bea944d817f63520266a9c1ea63cd337abd6cd5
AlexaSky <alexasky111@gmail.com>
2022-04-20 10:41:47 -0400

    git commands in console

--- src/Day_1/GroupProjectContinue.java
+++ src/Day_1/GroupProjectContinue.java
@@ -4,4 +4,5 @@ public class GroupProjectContinue {
     //created my local branch sasha
     //trying to merge it w/ the developer remote branch
     //trying git shortcut commands
+    //trying to use in console
 }
